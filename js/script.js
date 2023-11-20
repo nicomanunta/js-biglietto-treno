0245955fc693a465d81e8d6962fd707d20e73860
nicomanunta <nic.manun@gmail.com>
2023-11-20 14:35:31 +0100

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -5,5 +5,28 @@ console.log(numero_km)
 let eta = prompt("Inserire l'età")
 console.log(eta)
 
-let prezzo = numero_km * 0.21;
+let prezzo = numero_km * 0.21
 console.log(prezzo)
+let sconto;
+
+if (eta < 18){
+    sconto = numero_km * 0.21 * 20 / 100
+    console.log(sconto)
+}
+else{
+    prezzo = numero_km * 0.21;
+    
+}
+if (eta > 65){
+    sconto = numero_km * 0.21 * 40 / 100
+    console.log(sconto)
+}
+else{
+    prezzo = numero_km * 0.21;
+    
+}
+
+let prezzo_finale = prezzo - sconto
+
+let n = prezzo_finale.toFixed(2)
+console.log(n)
