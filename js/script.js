75f69464e22b5500ad26b5b8cbb08b8baf24afd6
nicomanunta <nic.manun@gmail.com>
2023-11-20 15:07:02 +0100

    add commenti e  nomi variabili, esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,32 +1,39 @@
 // prompt per richiedere numero di km da percorrere
 let numero_km = prompt("Inserire il numero di km da percorrere?")
 console.log(numero_km)
-
+// prompt per richiedere l'età
 let eta = prompt("Inserire l'età")
 console.log(eta)
+// calcolare il prezzo del viaggio moltiplicando il numero dei km per il prezzo al km 0.21 € 
+let biglietto = numero_km * 0.21
+console.log(biglietto)
 
-let prezzo = numero_km * 0.21
-console.log(prezzo)
 let sconto;
-
+let message;
 if (eta < 18){
-    sconto = numero_km * 0.21 * 20 / 100
+    message = "Hai diritto ad uno sconto del 20%"
+    sconto = biglietto * 20 / 100
     console.log(sconto)
+    
 }
 else{
-    prezzo = numero_km * 0.21;
+    biglietto = numero_km * 0.21;
     
 }
 if (eta > 65){
-    sconto = numero_km * 0.21 * 40 / 100
+    message = "Hai diritto ad uno sconto del 40%"
+    sconto = biglietto * 40 / 100
     console.log(sconto)
 }
 else{
-    prezzo = numero_km * 0.21;
+    biglietto = numero_km * 0.21;
     
 }
+// calcolare prezzo finale facendo una sottrazione tra prezzo intero e sconto se necessario
+let biglietto_scontato = biglietto - sconto
 
-let prezzo_finale = prezzo - sconto
+// limitare le cifre decimali fino a centesimi
+let biglietto_finale = biglietto_scontato.toFixed(2)
+console.log(biglietto_finale)
 
-let n = prezzo_finale.toFixed(2)
-console.log(n)
+// document.getElementById("prezzo").innerHTML = biglietto_finale;
