1f00dfb616100344f6f88f6a203eae27735b4b8b
nicomanunta <nic.manun@gmail.com>
2023-11-20 14:07:10 +0100

    aggiunto numero km, età e calcolo prezzo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// prompt per richiedere numero di km da percorrere
+let numero_km = prompt("Inserire il numero di km da percorrere?")
+console.log(numero_km)
+
+let eta = prompt("Inserire l'età")
+console.log(eta)
+
+let prezzo = numero_km * 0.21;
+console.log(prezzo)
